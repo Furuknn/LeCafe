07b3362e14a9e63c5b59f29eb251555741d130d3
Furkna <erbilginfurkan@gmail.com>
2024-03-05 01:14:11 +0300

    Version 1.1 (Fixed Gamebreaking Bugs)
    -Fixed the issue where microwave didn't work again after 1 time use
    
    - Fixed the issue where customers pile up and cause the game to soft lock. (they don't collide with each other anymore)
    
    - Fixed the bug where the player model in the main menu could be controlled by the player.
    
    - Adjusted windows' locations.
    
    - Added new invisible walls to prevent player from going out of the map boundries
    
    - Fixed the issue where player could rotate the model even though the game was paused.

---
+++ LeCafe (1.1)/Assets/Scripts/PlayerInteractions.cs
@@ -0,0 +1,184 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    #region Variables
+
+    private GameObject currentInteractable; // Currently interactable object within range
+
+    [SerializeField] private GameObject holdPizza;
+    [SerializeField] private GameObject holdCoffee;
+    [SerializeField] private GameObject holdCake;
+
+    [SerializeField] private GameObject timePanel;
+    [SerializeField] private GameObject infoPanel;
+
+    [SerializeField] private TextMeshProUGUI infoText;
+    [SerializeField] private TextMeshProUGUI timeText;
+
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] private CustomerBehaviour customerBehaviour;
+
+    [SerializeField] private GameManager gameManager;
+
+    private float pizzaHeatTime = 8f;
+    private float coffeeHeatTime = 3f;
+
+    private const int COFFEE_COST = 30;
+    private const int DESSERT_COST = 20;
+    private const int PIZZA_COST = 50;
+
+    private bool handsFull=false;
+    bool microwaveFull = false;
+    bool pizzaHeated=false;
+    bool coffeeHeated = false;
+    bool dessertReady=false;
+
+    #endregion
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            currentInteractable = other.gameObject;
+            infoPanel.SetActive(true);
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Interactable"))
+        {
+            infoPanel.SetActive(false);
+            currentInteractable = null;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        // Check for player input to interact with objects
+        if (Input.GetKeyDown(KeyCode.E) && currentInteractable != null)
+        {
+            InteractWithObject(currentInteractable);
+        }
+    }
+
+    void InteractWithObject(GameObject interactable)
+    {
+        customerBehaviour = interactable.GetComponent<CustomerBehaviour>();
+
+        if (customerBehaviour == null)
+        {
+            Debug.Log("Cannot find customer behaviour script");
+        }
+
+        if (interactable.name=="Pizza" &&!handsFull)
+        {
+            coffeeHeated = false;
+            holdPizza.SetActive(true);
+            handsFull=true;
+            infoText.text = "Microwave the pizza";
+        }
+        if (interactable.name == "Coffee Machine" && !handsFull)
+        {
+            handsFull = true;
+            playerController.enabled=false;
+            StartCoroutine(Prepare(coffeeHeatTime));
+        }
+        if (interactable.name == "Cake" && !handsFull)
+        {
+            coffeeHeated = false;
+            holdCake.SetActive(true);
+            handsFull = true;
+            dessertReady=true;
+        }
+        if (interactable.name == "Microwave")
+        {
+            //microwaving
+            if (holdPizza.activeSelf && !microwaveFull && !pizzaHeated)
+            {
+                holdPizza.SetActive(false);
+                handsFull = false;
+                microwaveFull = true;
+                pizzaHeated = true;
+                StartCoroutine(Prepare(pizzaHeatTime));
+                
+            }
+            else if(!timePanel.activeSelf && microwaveFull && pizzaHeated)//taking out from microwave
+            {
+                holdPizza.SetActive(true);
+                handsFull = true;
+                microwaveFull = false;
+                
+            }
+        }
+
+        if (interactable.name == "CoffeeCUSTOMER(Clone)" && handsFull && coffeeHeated)
+        {
+            holdCoffee.SetActive(false);
+            handsFull=false;
+
+            Pay(COFFEE_COST);
+            
+
+            Debug.Log("Coffee Given");
+        }
+        if (interactable.name == "DessertCUSTOMER(Clone)" && handsFull && dessertReady)
+        {
+            holdCake.SetActive(false);
+            handsFull = false;
+
+            Pay(DESSERT_COST);
+            
+        }
+        if (interactable.name == "PizzaCUSTOMER(Clone)" && handsFull && pizzaHeated)
+        {
+            holdPizza.SetActive(false);
+            handsFull = false;
+            pizzaHeated = false;
+
+            Pay(PIZZA_COST);
+            
+        }
+    }
+
+    IEnumerator Prepare(float time)
+    {
+        Debug.Log("Preparing");
+        timePanel.SetActive(true);
+        for (float i = time; i >= 0; i--)
+        {
+            timeText.text = "" + i;
+            yield return new WaitForSeconds(1f);
+            Debug.Log(i);
+        }
+        //yield return new WaitForSeconds(time);
+        timePanel.SetActive(false);
+
+        if (time==pizzaHeatTime)
+        {
+            infoText.text = "Pizza Heated!";
+        }
+        else if (time==coffeeHeatTime)
+        {
+            coffeeHeated=true;
+            holdCoffee.SetActive(true);
+            infoText.text = "Coffee Heated!";
+            playerController.enabled = true;
+        }
+
+    }
+
+    private void Pay(int cost)
+    {
+        gameManager.currentMoney += cost;
+        customerBehaviour.isHungry = false;
+    }
+}
